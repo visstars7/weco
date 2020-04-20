324c7c336b9b1acb1b60ec854eaa2302a95f895a
root <root@localhost.localdomain>
2020-04-19 20:18:32 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>We.Co || We.Coffe</title>
+
+    <!-- bootstrap 4 -->
+    <link rel="stylesheet" href="resource/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="resource/css/landing_page.css">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="resource/img/Weco.png" type="image/x-icon">
+
+</head>
+<body>
+
+    <div id="header-wave">
+        
+        <img id="header-wave-image" class="image-fluid" src="resource/img/wave 1.svg" alt="wave.png">
+
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div id="mark-kiri" class="col-md-6 col-sm-12 col-xs-12">
+                
+                <img id="header-wave-image" class="image-fluid" src="resource/img/undraw_coffee_break_j3of 1.svg" alt="wave.png">
+
+            </div>
+
+            <div id="mark-kanan" class="col-md-6 col-sm-12 col-xs-12">
+
+                <div id="bold-font-logo">
+
+                    <h2 class="ubuntu-font"><span>We.</span>Co</h2>
+
+                </div>
+
+                <div id="bold-font-logo">
+
+                    <p class="ubuntu-font paragraf">
+                        Bagi sebagian besar masyarakat indonesia maupun dunia, kopi sudah seperti menjadi teman hidup. Melewati hari tanpa menyeruput kopi rasanya seperti ada yang kurang. Urusan minum kopi juga merupakan hal yang sangat personal dimana masing masing orang memiliki kesukaan masing-masing. Ada yang suka kopi asli tanpa campuran susu atau kremer, dan ada juga yang sebaliknya.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="button">
+
+            <a href="app/index.php?page=home" class="button-landing ubuntu-font" >Let's Begin</a>
+
+        </div>
+
+    </div>
+
+
+<script src="resource/jquery-3.4.1.min.js" ></script>
+<script src="resource/popper/popper.min.js" ></script>
+<script src="resource/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
