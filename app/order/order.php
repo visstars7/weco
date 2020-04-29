37bb842d929a48f948aef1e285b816528fd869f4
visstars7 <havisikkubaru@gmail.com>
2020-04-29 15:36:21 -0400

    pembuatan proses pesanan dan penyempurnaan order_detail

--- app/order/order.php
+++ app/order/order.php
@@ -1,6 +1,11 @@
 <?php 
 
 // session_start();
+
+if(!isset($_SESSION['keranjang']) || !isset($_SESSION['user'])){
+    Header('Location:index.php?page=home');
+}
+
 include_once 'helper/helper.php';
 
 $pdo  = PDO();
@@ -16,168 +21,324 @@ $pdoStatementViewKec = $pdo->prepare("SELECT * FROM tbkecamatan WHERE status = '
 $pdoStatementViewKec->execute();
 $kec = $pdoStatementViewKec->fetchAll(PDO::FETCH_ASSOC);
 
+// filtering jika array yang di $_GET ada dalam produk
+$arrayID = [];
+foreach($_SESSION['keranjang'] AS $key){
+    $arrayID[] = $key['produk_id']; 
+}
+
+
 $no = 1;
 
 // var_dump($_SESSION['keranjang']);
 
 ?>
 
-<div class="row">
+<?php if(isset($_GET['id'])): ?>
 
-    <div class="col-md-4">
+    <?php if(in_array($_GET['id'],$arrayID) == true): ?>
 
-        <div class="product-frame">
+        <div class="row">
 
-            <div class="title-header-product">
-            
-                <h3>Produk yang anda beli</h3>
+            <div class="col-md-4">
 
-            </div>
+                <div class="product-frame">
 
-            <div class="content-product table-responsive">
-            
-                <table class="table table-striped table-hover table-sm ">
-
-                    <thead>
-
-                        <tr>
-                            <th>No.</th>
-                            <th>Nama Product</th>
-                            <th>Gambar</th>
-                            <th>Qty</th>
-                            <th>Total harga</th>
-
-                        </tr>
-
-                    </thead>
-
-                    <tbody>
-                        <?php if(isset($_GET['id'])): ?>
-                        <?php 
-                            $id = intval($_GET['id']);
-                            $keranjang = $_SESSION['keranjang'];
-                            
-                        ?>
-                        <tr>
-                            <td><?= $no ?></td>
-                            <td><?= $keranjang[$id]['nama_produk'] ?></td>
-                            <td><img style="width:150px" src="order/image.php?id=<?=$keranjang[$id]['produk_id']?>" alt="<?= $keranjang[$id]['nama_produk'] ?>"></td>
-                            <td><?= $keranjang[$id]['qty'] ?></td>
-                            <td><?=rupiah($keranjang[$id]['qty']*$keranjang[$id]['harga'])?></td>
-                            
-                        </tr>
-
-
-                        <?php else: ?>
-                            <?php foreach($_SESSION['keranjang'] AS $row): ?>
-                        <tr>
-                            <td><?= $no ?></td>
-                            <td><?= $row['nama_produk'] ?></td>
-                            <td><img style="width:150px" src="order/image.php?id=<?=$row['produk_id']?>" alt="<?= $row['nama_produk'] ?>"></td>
-                            <td><?= $row['qty'] ?></td>
-                            <td><?=rupiah($row['qty']*$row['harga'])?></td>
-                            
-                        </tr>
-                            <?php $no++ ?>
-                            <?php endforeach; ?>
-
-                        <?php endif; ?>
+                    <div class="title-header-product">
                     
-                    </tbody>
+                        <h3 class="ubuntu-font text-center">Produk yang anda beli</h3>
 
-                </table>
-            
-            </div>
-        
-        </div>
+                    </div>
 
-    </div>
+                    <div class="content-product table-responsive">
+                    
+                        <table class="table table-striped table-hover table-sm text-brown abel-font">
 
-    <div class="col-md-8">
+                            <thead>
 
-        <div class="stranger-frame">
+                                <tr>
+                                    <th>No.</th>
+                                    <th class="text-center">Product</th>
+                                    <th class="text-center">Gambar</th>
+                                    <th>Qty</th>
+                                    <th class="text-center">harga</th>
 
-            <form action="" method="post">
-            
-                <ul>
+                                </tr>
+
+                            </thead>
+
+                            <tbody>
+                                <?php if(isset($_GET['id'])): ?>
+                                <?php 
+                                    $id = intval($_GET['id']);
+                                    $keranjang = $_SESSION['keranjang'];
+                                    
+                                ?>
+                                <tr>
+                                    <td><?= $no ?></td>
+                                    <td><?= $keranjang[$id]['nama_produk'] ?></td>
+                                    <td><img style="width:150px" src="order/image.php?id=<?=$keranjang[$id]['produk_id']?>" alt="<?= $keranjang[$id]['nama_produk'] ?>"></td>
+                                    <td class="text-center"><?= $keranjang[$id]['qty'] ?></td>
+                                    <td><?=rupiah($keranjang[$id]['qty']*$keranjang[$id]['harga'])?></td>
+                                    
+                                </tr>
+
+                                <?php endif; ?>
+                            
+                            </tbody>
+
+                        </table>
+                    
+                    </div>
                 
-                    <li>
-                    
-                        <label for='penerima'>Nama Penerima</label>
-                        <input type='text' name='penerima' id='penerima' placeholder='penerima'>
-                    
-                    </li>
+                </div>
 
-                    <li>
-                    
-                        <label for='nomor'>Telp penerima</label>
-                        <input type='number' max-length="12" name='nomor' id='nomor' placeholder='nomor'>
-                    
-                    </li>
+            </div>
 
-                    <li>
-                    
-                        <label for='penerima'>Alamat Penerima</label>
-                        <textarea name="alamat" id="alamat"></textarea>
-                    
-                    </li>
+            <div class="col-md-8">
 
-                    <li>
-                    
-                        <label for='kota'>Kota Penerima</label>
-                        <select name="kota" id="kota">
-                            <?php foreach($kota AS $key): ?>
-                                <option value="<?=$key['kota_id']?>"><?= $key['nama_kota'] ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    
-                    </li>
+                <div class="stranger-frame">
 
-                    <li>
+                    <form action="order/orderInsert.php" method="post">
                     
-                        <label for='kecamatan'>kecamatan Penerima</label>
-                        <select name="kecamatan" id="kecamatan">
-
-                            <?php foreach($kec AS $key): ?>
-                                <option value="<?=$key['kec_id']?>"><?= $key['nama_kec'] ?></option>
-                            <?php endforeach; ?>
+                        <ul class='form-group'>
                         
-                        </select>
-                    
-                    </li>
+                            <li>
+                            
+                                <label for='penerima'>Nama Penerima</label>
+                                <input class="form-control" type='text' name='penerima' id='penerima' placeholder='penerima'>
+                                <input class="form-control" type='hidden' name='id' id='id' placeholder='penerima' value="<?=$_GET['id']?>">
+                            
+                            </li>
 
-                    <li>
-                    
-                        <label for='provinsi'>provinsi Penerima</label>
-                        <select name="provinsi" id="provinsi">
-                            <?php foreach($provinsi AS $key): ?>
-                                <option value="<?=$key['prov_id']?>"><?= $key['nama_prov'] ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    
-                    </li>
+                            <li>
+                            
+                                <label for='nomor'>Telp penerima</label>
+                                <input class="form-control" type='number' max-length="12" name='nomor' id='nomor' placeholder='nomor'>
+                            
+                            </li>
 
-                    <li>
-                    
-                        <label for='kode pos'>Kode Pos</label>
-                        <input type='number' name='kode pos' id='kode pos' placeholder='kode pos'>
-                    
-                    </li>
+                            <li>
+                            
+                                <label for='penerima'>Alamat Penerima</label>
+                                <textarea class="form-control" name="alamat" id="alamat"></textarea>
+                            
+                            </li>
 
-                    <li>
+                            <li class="form-row">
+                                <div class="col-4">
+
+                                    <label for='kota'>Kota </label>
+                                    <select class="form-control" name="kota" id="kota">
+                                        <?php foreach($kota AS $key): ?>
+                                            <option value="<?=$key['kota_id']?>"><?= $key['nama_kota'] ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                
+                                </div>
+                                            
+                                <div class="col-4">
+                                
+                                    <label for='kecamatan'>kecamatan</label>
+                                    <select class="form-control" name="kecamatan" id="kecamatan">
+
+                                        <?php foreach($kec AS $key): ?>
+                                            <option value="<?=$key['kec_id']?>"><?= $key['nama_kec'] ?></option>
+                                        <?php endforeach; ?>
+                                    
+                                    </select>
+                                
+                                </div>
+                                
+                                <div class="col-4">
+
+                                    <label for='provinsi'>provinsi</label>
+                                    <select class="form-control" name="provinsi" id="provinsi">
+                                        <?php foreach($provinsi AS $key): ?>
+                                            <option value="<?=$key['prov_id']?>"><?= $key['nama_prov'] ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+
+                                </div>
+
+                            <li>
+                            
+                                <label for='kode pos'>Kode Pos</label>
+                                <input class="form-control" type='number' name='kodepos' id='kode pos' placeholder='kode pos'>
+                            
+                            </li>
+
+                            <li class="mt-2 text-center">
+                        
+                                <button class="btn btn-info w-50" name="data-penerima" >Kirim</button>
+                        
+                            </li>
+                        
+                        
+                        </ul>
                     
-                        <button class="btn btn-info" name="data-penerima" >Kirim</button>
-                    
-                    </li>
-                    
-                
-                </ul>
-            
-            </form>
+                    </form>
+
+                </div>
+
+            </div>
+
 
         </div>
 
-    </div>
+    <?php else: ?>
+
+    <?php 
+
+        Header('Location:index.php?page=home');
+        
+    ?>
+
+    <?php endif; ?>
+
+<?php else: ?>
+
+    <div class="row">
+
+        <div class="col-md-4">
+
+            <div class="product-frame">
+
+                <div class="title-header-product">
+                
+                    <h3 class="ubuntu-font text-center">Produk yang anda beli</h3>
+
+                </div>
+
+                <div class="content-product table-responsive">
+                
+                    <table class="table table-striped table-hover table-sm text-brown abel-font">
+
+                        <thead>
+
+                            <tr>
+                                <th>No.</th>
+                                <th class="text-center">Product</th>
+                                <th class="text-center">Gambar</th>
+                                <th>Qty</th>
+                                <th class="text-center">harga</th>
+
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+
+                            <?php foreach($_SESSION['keranjang'] AS $row): ?>
+                            <tr>
+                                <td><?= $no ?></td>
+                                <td><?= $row['nama_produk'] ?></td>
+                                <td><img style="width:150px" src="order/image.php?id=<?=$row['produk_id']?>" alt="<?= $row['nama_produk'] ?>"></td>
+                                <td class="text-center"><?= $row['qty'] ?></td>
+                                <td><?=rupiah($row['qty']*$row['harga'])?></td>
+                                
+                            </tr>
+                                <?php $no++ ?>
+                                <?php endforeach; ?>
+                        
+                        </tbody>
+
+                    </table>
+                
+                </div>
+            
+            </div>
+
+        </div>
+
+        <div class="col-md-8">
+
+            <div class="stranger-frame">
+
+                <form action="order/orderInsert.php" method="post">
+                
+                <ul class='form-group'>
+                        
+                        <li>
+                        
+                            <label for='penerima'>Nama Penerima</label>
+                            <input class="form-control" type='text' name='penerima' id='penerima' placeholder='penerima'>
+                        
+                        </li>
+
+                        <li>
+                        
+                            <label for='nomor'>Telp penerima</label>
+                            <input class="form-control" type='number' max-length="12" name='nomor' id='nomor' placeholder='nomor'>
+                        
+                        </li>
+
+                        <li>
+                        
+                            <label for='penerima'>Alamat Penerima</label>
+                            <textarea class="form-control" name="alamat" id="alamat"></textarea>
+                        
+                        </li>
+
+                        <li class="form-row">
+                            <div class="col-4">
+
+                                <label for='kota'>Kota </label>
+                                <select class="form-control" name="kota" id="kota">
+                                    <?php foreach($kota AS $key): ?>
+                                        <option value="<?=$key['kota_id']?>"><?= $key['nama_kota'] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            
+                            </div>
+                                        
+                            <div class="col-4">
+                            
+                                <label for='kecamatan'>kecamatan</label>
+                                <select class="form-control" name="kecamatan" id="kecamatan">
+
+                                    <?php foreach($kec AS $key): ?>
+                                        <option value="<?=$key['kec_id']?>"><?= $key['nama_kec'] ?></option>
+                                    <?php endforeach; ?>
+                                
+                                </select>
+                            
+                            </div>
+                            
+                            <div class="col-4">
+
+                                <label for='provinsi'>provinsi</label>
+                                <select class="form-control" name="provinsi" id="provinsi">
+                                    <?php foreach($provinsi AS $key): ?>
+                                        <option value="<?=$key['prov_id']?>"><?= $key['nama_prov'] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+
+                            </div>
+
+                        <li>
+                        
+                            <label for='kode pos'>Kode Pos</label>
+                            <input class="form-control" type='number' name='kodepos' id='kode pos' placeholder='kode pos'>
+                        
+                        </li>
+
+                        <li class="mt-2 text-center">
+                        
+                            <button class="btn btn-info w-50" name="data-penerima" >Kirim</button>
+                        
+                        </li>
+                        
+                    
+                    </ul>
+                
+                </form>
+
+            </div>
+
+        </div>
 
 
-</div>
+        </div>
+
+<?php endif; ?>
---
+++ app/order/orderInsert.php
@@ -0,0 +1,99 @@
+<?php 
+
+include_once '../helper/helper.php';
+
+$pdo = PDO();
+
+session_start();
+
+
+if(!isset($_SESSION['user']) || !isset($_SESSION['keranjang'])){
+    Header('Location:../index.php?page=home');
+}else{
+    
+    if(isset($_POST['penerima'])){
+        $nama = htmlspecialchars($_POST['penerima']);
+        $nomor =  htmlspecialchars($_POST['nomor']);
+        $alamat =  htmlspecialchars($_POST['alamat']);
+        $kec =  htmlspecialchars($_POST['kecamatan']);
+        $kota =  htmlspecialchars($_POST['kota']);
+        $prov =  htmlspecialchars($_POST['provinsi']);
+        $kodePos =  htmlspecialchars($_POST['kodepos']);
+        $harga = 0;
+        
+        $pdoStatementInsert = $pdo->prepare("INSERT INTO tborder (`order_id`,`nama`,`alt_penerima`,`kota_id`,`prov_id`,`kec_id`,`kodepos`,`phone`) VALUES (NULL,:nama,:alamat,:kota,:prov,:kec,:kodePos,:phone)");
+        
+        $pdoStatementInsert->bindParam(":nama",$nama);
+        $pdoStatementInsert->bindParam(":phone",$nomor);
+        $pdoStatementInsert->bindParam(":alamat",$alamat);
+        $pdoStatementInsert->bindParam(":kec",$kec);
+        $pdoStatementInsert->bindParam(":kota",$kota);
+        $pdoStatementInsert->bindParam(":prov",$prov);
+        $pdoStatementInsert->bindParam(":kodePos",$kodePos);
+        
+        if($pdoStatementInsert->execute()){
+            $orderId =  intval($pdo->lastInsertId());
+            echo $harga;
+            $jam =  date("h");
+            $jam +=1;
+            $jamTanggal = date("Y-m-d")." ".$jam.date(":i:s");
+            $userID = intval($_SESSION['user']);
+            
+            if(isset($_POST['id'])){
+                $id = $_POST['id'];
+                // echo $id;
+                $harga = intval($_SESSION['keranjang'][$id]['harga']*$_SESSION['keranjang'][$id]['qty']);
+                $produkID = intval($_SESSION['keranjang'][$id]['produk_id']);
+                $qty = $_SESSION['keranjang'][$id]['qty'];
+                $pdoStatementInsert  = $pdo->prepare("INSERT INTO tbonproses (`proses_id`,`produk_id`,`order_id`,`user_id`,`qty`,`total`,`tanggal_order`,`status`) VALUES (NULL,:produkID,:orderId,:userID,:qty,:harga,:jamTanggal,'0')");
+                $pdoStatementInsert->bindParam(":produkID",intval($produkID));
+                $pdoStatementInsert->bindParam(":orderId",intval($orderId));
+                $pdoStatementInsert->bindParam(":userID",intval($userID));
+                $pdoStatementInsert->bindParam(":qty",intval($qty));
+                $pdoStatementInsert->bindParam(":harga",intval($harga));
+                $pdoStatementInsert->bindParam(":jamTanggal",$jamTanggal);
+                if($pdoStatementInsert->execute()){
+                    unset($_SESSION['keranjang'][$id]);
+                    Header('Location:checkout.php');
+                }else{
+                    echo "gagal";
+                }
+            }
+            else{
+
+                foreach($_SESSION['keranjang'] AS $key => $value){
+                    $harga += intval($value['harga']*$value['qty']);
+                }
+                
+                foreach ($_SESSION['keranjang'] AS $key => $value){
+                    $produkID = intval($value['produk_id']);
+                    $harga;
+                    $qty = $value['qty'];
+                    $pdoStatementInsert  = $pdo->prepare("INSERT INTO tbonproses (`proses_id`,`produk_id`,`order_id`,`user_id`,`qty`,`total`,`tanggal_order`,`status`) VALUES (NULL,:produkID,:orderId,:userID,:qty,:harga,:jamTanggal,'0')");
+                    $pdoStatementInsert->bindParam(":produkID",intval($produkID));
+                    $pdoStatementInsert->bindParam(":orderId",intval($orderId));
+                    $pdoStatementInsert->bindParam(":userID",intval($userID));
+                    $pdoStatementInsert->bindParam(":qty",intval($qty));
+                    $pdoStatementInsert->bindParam(":harga",intval($harga));
+                    $pdoStatementInsert->bindParam(":jamTanggal",$jamTanggal);
+                    if($pdoStatementInsert->execute()){
+                        echo "sukses";
+                    }else{
+                        echo "gagal";
+                    }      
+                }
+                unset($_SESSION['keranjang']);
+                Header('Location:checkout.php');
+            }
+
+        }else{
+            Header('Location:../index.php?page=home');
+        }
+    
+    }else{
+        Header('Location:../index.php?page=home');
+    }
+}
+
+
+?>
